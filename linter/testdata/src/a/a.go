b5b63755a91950235cf577497ecc8913d863c680
takaaa220 <taaaaaaaaaa220@gmail.com>
2025-02-22 10:54:47 +0900

    remove replace

--- linter/testdata/src/a/a.go
+++ linter/testdata/src/a/a.go
@@ -22,9 +22,3 @@ func badUpdate(ctx context.Context, db *bun.DB, user *User) (sql.Result, error) 
 func goodUpdate(ctx context.Context, db *bun.DB, user *User) (sql.Result, error) {
 	return bunwithoptimistic.WithOptimistic(db.NewUpdate().Model(user)).Exec(ctx)
 }
-
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
-}
